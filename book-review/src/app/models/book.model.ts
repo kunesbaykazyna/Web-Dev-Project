36c232bfb98deef5116b43693657c2e2c0a7ca65
Dilnaz Tubekbayeva <dilnaz_tubekbaeva@mail.ru>
2026-04-12 19:51:24 +0600

    add Book and Author interfaces

---
+++ book-review/src/app/models/book.model.ts
@@ -0,0 +1,23 @@
+export interface Author {
+    id: number;
+    first_name: string;
+    last_name: string;
+    full_name: string;
+    bio: string;
+    birth_date: string;
+    book_count: number;
+}
+
+export interface Book {
+    id: number;
+    title: string;
+    author: Author;
+    author_name: string;
+    description: string;
+    published_date: string;
+    isbn: string;
+    genre: string;
+    cover_image: string;
+    created_at: string;
+    average_rating: number | null;
+}
