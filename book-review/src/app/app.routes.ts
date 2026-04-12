da0d5f2e91cca173761a4b37618b3cd8d8d8fef2
Dilnaz Tubekbayeva <dilnaz_tubekbaeva@mail.ru>
2026-04-12 20:24:46 +0600

    configure routing for books list and detail pages

--- book-review/src/app/app.routes.ts
+++ book-review/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { BooksList } from './pages/books-list/books-list';
-import { BookDetail } from './pages/book-detail/book-detail';
+import { BooksListComponent } from './pages/books-list/books-list';
+import { BookDetailComponent } from './pages/book-detail/book-detail';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProfileComponent } from './components/profile/profile.component';
@@ -8,14 +8,9 @@ import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
-    {
-        path: 'books',
-        component: BooksList
-    },
-    {
-        path: 'books/:id',
-        component: BookDetail
-    },
+    { path: '', redirectTo: 'books', pathMatch: 'full' },
+    { path: 'books', component: BooksListComponent },
+    { path: 'books/:id', component: BookDetailComponent },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
