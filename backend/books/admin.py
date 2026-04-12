283598d1a25737949daabd12b4bcb7720f25c6c7
Dilnaz Tubekbayeva <dilnaz_tubekbaeva@mail.ru>
2026-04-12 19:25:22 +0600

    register Author and Book in Django admin

--- backend/books/admin.py
+++ backend/books/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Author, Book
 
-# Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'birth_date']
+    search_fields = ['first_name', 'last_name']
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'genre', 'published_date']
+    list_filter = ['genre']
+    search_fields = ['title', 'author__first_name', 'author__last_name']
